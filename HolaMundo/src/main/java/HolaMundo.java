15dc2aa6e5ea04da75071b823c43ff902f38ee49
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-08 12:25:46 -0500

    Hola Mundo Java

---
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,11 @@
+/*
+ * Muestra por pantalla la frase Hola Mundo
+ *
+ * @author Luigi Arrieta
+ */
+public class HolaMundo { // Clase Principal
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
