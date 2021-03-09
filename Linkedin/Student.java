b7566dd756a7e71b2cda83fc6f00f0dfb05e62c6
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-09 18:50:27 -0500

    Data type in Java

---
+++ Linkedin/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+	public static void main(String[] args){
+		int studentAge = 14;
+		double studentGrade = 3.45;
+		char studentFirstInitial = 'L';
+		char studentLastInitial = 'A';
+		boolean studentHasPerfectAttendance = true;
+
+		System.out.println(studentAge);
+		System.out.println(studentGrade);
+		System.out.println(studentFirstInitial);
+		System.out.println(studentLastInitial);
+		System.out.println(studentHasPerfectAttendance);
+	}
+}
