753bd7903903b98e9709a0d43ecb61beaf99a93d
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-10 16:00:57 -0500

    Class Persona Java

---
+++ Linkedin/Basic3/Persona.java
@@ -0,0 +1,15 @@
+public class Persona {
+	String name;
+	String lastName;
+	int age;
+
+	// Constructor
+	public Persona(){
+	}
+
+	public Persona(String name, String lastName, int age){
+		this.name = name;
+		this.lastName = lastName;
+		this.age = age;
+	}
+}
