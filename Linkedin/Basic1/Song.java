b473546254ca740ed408dfe93f722a4717e0a0ca
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-10 07:42:40 -0500

    Basics One Java

---
+++ Linkedin/Basic1/Song.java
@@ -0,0 +1,8 @@
+public class Song {
+	public static void main(String[] args){
+		String songName = "El culpable soy yo";
+		System.out.println(songName);
+		System.out.println(songName.charAt(0));
+		System.out.println(songName.charAt(6));
+	}
+}
---
+++ Linkedin/Basic1/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+	public static void main(String[] args){
+		String name = "Luigi";
+		String lastName = "Arrieta";
+
+		System.out.println("Name Full: " + name + " " + lastName);
+	}
+}
