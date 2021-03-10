897978fae973e59ad2e301483f9bfc26b0e4b6b7
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-10 07:43:40 -0500

    Move to file Basics One

--- Linkedin/Strings.java
+++
@@ -1,8 +0,0 @@
-public class Strings {
-	public static void main(String[] args){
-		String name = "Luigi";
-		String lastName = "Arrieta";
-
-		System.out.println("Name Full: " + name + " " + lastName);
-	}
-}
