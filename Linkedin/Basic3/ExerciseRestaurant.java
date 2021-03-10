81cff13eaaecb7dab52ae90c20e9ee102f209fdc
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-10 12:34:14 -0500

    Function Return

--- Linkedin/Basic3/ExerciseRestaurant.java
+++ Linkedin/Basic3/ExerciseRestaurant.java
@@ -6,18 +6,19 @@
 public class ExerciseRestaurant {
 
 	// Function Calc Price
-	public static void calcPrice(double precio){
+	public static double calcPrice(double precio){
 		double iva = precio * 19 / 100; // Ival del 19%
 		double precioFinal = precio + iva;
-		System.out.printf("Debes Pagar %.0f:\n", precioFinal);
+		return precioFinal;
+		//System.out.printf("Debes Pagar %.0f:\n", precioFinal);
 	}
 
 	// Function Main
 	public static void main(String[] args){
 		// Call function
-		calcPrice(23500);
-		calcPrice(4500);
-		calcPrice(12300);
-		calcPrice(500);
+		System.out.println("Debes pagar: " + calcPrice(23500));
+		System.out.println("Debes pagar: " + calcPrice(4500));
+		System.out.println("Debes pagar: " + calcPrice(12300));
+		System.out.println("Debes pagar: " + calcPrice(500));
 	}
 }
