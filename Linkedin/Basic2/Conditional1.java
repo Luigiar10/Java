16b2a3d6a9ed61132c72ec771dda0f22d4cde10d
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-10 09:24:42 -0500

    While in Java

---
+++ Linkedin/Basic2/Conditional1.java
@@ -0,0 +1,15 @@
+/*
+* While loop (Display nums of zero to nine)
+*
+* @author Luigi Arrieta
+*/
+public class Conditional1 {
+	public static void main(String[] args) {
+		// While loop in Java
+		int num = 0;
+		while(num < 10){
+			System.out.println("Num: " + num);
+			num++;
+		}
+	}
+}
