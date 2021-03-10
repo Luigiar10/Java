83ed26b78402d0897a827368af8e4771d128271c
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-10 08:11:28 -0500

    Basic Two

---
+++ Linkedin/Basic2/Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+	public static void main(String[] args){
+		// Operators are:
+		// (+ , *, /, -, %)
+		int num1 = 5, num2 = 4;
+		// Operator +
+		int resultPlus = num1 + num2;
+		System.out.println("Sum: " + resultPlus);
+		// Operator *
+		int resultMul = num1 * num2;
+		System.out.println("Mul: " + resultMul);
+		// Operator /
+		int resultDiv = num1 / num2;
+		System.out.println("Div: " + resultDiv);
+		// Operator -
+		int resultMin = num1 - num2;
+		System.out.println("Minus: " + resultMin);
+		// Operator %
+		int resultMod = num1 % num2;
+		System.out.println("Module: " + resultMod);
+	}
+}
