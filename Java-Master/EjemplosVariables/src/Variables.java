a040a5497394abd76ca599d78c63dc86d8967909
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-20 08:51:10 -0500

    Ejemplos Variables

---
+++ Java-Master/EjemplosVariables/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args){
+        String nombre = "Luigi";
+        System.out.println("Nombre: " + nombre);
+        int edad = 20;
+        System.out.println("Edad: " + edad);
+        float sueldo = 2500000;
+        System.out.println("Sueldo: " + sueldo);
+        boolean trabajo = true;
+        System.out.println("Trabajo: " + trabajo);
+        char inicialNombre = 'L';
+        System.out.println("Letra Inicial Nombre: " + inicialNombre);
+        /** Desde la version 10 se puede usar la palabra var, esto sirve para asignar el tipo
+         * de dato dependiedno del valor */
+        var edad2 = 19;
+        System.out.println("edad2: " + edad2);
+    }
+}
