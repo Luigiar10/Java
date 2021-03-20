f5c4c838d89601bc6a3d3ab2694bee1453a36ace
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-20 16:16:52 -0500

    Datos Primtivos Java

--- Java-Master/Primitivos/src/Primitivos.java
+++ Java-Master/Primitivos/src/Primitivos.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Primitivos {
     public static void main(String[] args){
         // Tipo Byte
@@ -7,31 +5,31 @@ public class Primitivos {
         System.out.println("Tipo byte corresponde en bytes a " + Byte.SIZE);
         System.out.println("El valor maximo de un byte es " + Byte.MAX_VALUE);
         System.out.println("El valor minimo de un byte es " + Byte.MIN_VALUE);
-        System.out.println("");
+        System.out.println("***********************************");
         // Tipo Short
         System.out.println("Tipo Short corresponde en byte a " + Short.BYTES);
         System.out.println("Tipo Short corresponde en bytes a " + Short.SIZE);
         System.out.println("El valor maximo de un short es " + Short.MAX_VALUE);
         System.out.println("El valor minimo de un short es " + Short.MIN_VALUE);
-        System.out.println("");
+        System.out.println("***********************************");
         // Tipo int
         System.out.println("Tipo int corresponde en byte a " + Integer.BYTES);
         System.out.println("Tipo int corresponde en bytes a " + Integer.SIZE);
         System.out.println("El valor maximo de un int es " + Integer.MAX_VALUE);
         System.out.println("El valor minimo de un int es " + Integer.MIN_VALUE);
-        System.out.println("");
+        System.out.println("***********************************");
         // Tipo Float
         System.out.println("Tipo float corresponde en byte a " + Float.BYTES);
         System.out.println("Tipo float corresponde en bytes a " + Float.SIZE);
         System.out.println("El valor maximo de un float es " + Float.MAX_VALUE);
         System.out.println("El valor minimo de un float es " + Float.MIN_VALUE);
-        System.out.println("");
+        System.out.println("***********************************");
         // Tipo Double
         System.out.println("Tipo double corresponde en byte a " + Double.BYTES);
         System.out.println("Tipo double corresponde en bytes a " + Double.SIZE);
         System.out.println("El valor maximo de un double es " + Double.MAX_VALUE);
         System.out.println("El valor minimo de un double es " + Double.MIN_VALUE);
-        System.out.println("");
+        System.out.println("***********************************");
         // Tipo Long
         System.out.println("Tipo long corresponde en byte a " + Long.BYTES);
         System.out.println("Tipo long corresponde en bytes a " + Long.SIZE);
