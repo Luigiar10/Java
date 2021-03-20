de8a17b5522d43c54de5833e53382a160af3922e
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-03-20 17:00:15 -0500

    Login Operadores Java

---
+++ Java-Master/Primitivos/src/OperadoresLogicos.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        String userName = "luigiar10";
+        String password = "123456";
+
+        System.out.println("Ingrese su Nombre de Usuario");
+        Scanner scanU = new Scanner(System.in);
+        String scanUser = scanU.next();
+
+        System.out.println("Ingrese su Contraseña");
+        Scanner scanP = new Scanner(System.in);
+        String scanPassword = scanP.next();
+
+        if (scanUser.equals(userName) && scanPassword.equals(password)){
+            System.out.println("Bienvenido al sistema");
+        } else {
+            System.out.println("Ha ocurrido un problema, verifique usuario y contraseña");
+        }
+    }
+}
